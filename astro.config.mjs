6292c8d3acf416c5103660843a9a4e4600591abb
mzaini30 <muhzaini30@gmail.com>
2023-08-05 11:24:43 -0700

    tambah uno css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
-import yaml from 'vite-plugin-yaml2';
-
+import { defineConfig } from "astro/config";
+import yaml from "vite-plugin-yaml2";
+import UnoCSS from "unocss/astro";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [yaml()]
+    plugins: [yaml()],
   },
-  integrations: [svelte()]
+  integrations: [svelte(), UnoCSS()],
 });
